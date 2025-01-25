042c5ac3c14eba7286cfda20198505a86a01d062
smalaca <ab13.krakow@gmail.com>
2025-01-25 22:55:50 +0100

    add simple test

---
+++ Refactoring.Web.Tests/Services/OrderServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Refactoring.Web.DomainModels;
+using Refactoring.Web.Services;
+
+namespace Refactoring.Web.Tests.Services;
+
+[TestClass]
+[TestSubject(typeof(OrderService))]
+public class OrderServiceTest
+{
+    [TestMethod]
+    public void ShouldProcessOrder()
+    {
+        var orderService = new OrderService(new Order());
+
+        orderService.ProcessOrder().ConfigureAwait(false);
+        var order = orderService.GetOrder();
+        
+        Assert.IsNull(order.Status);
+    }
+}
